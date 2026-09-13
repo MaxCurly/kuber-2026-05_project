4fd0d8aa6844553c930488599696b11f67ab9993
Maxim Kudryashov <m.v.kudryashov@ya.ru>
2026-09-13 18:33:15 +0300

    test

--- terraform/argocd-deploy/resources.tf
+++ terraform/argocd-deploy/resources.tf
@@ -3,7 +3,7 @@ resource "helm_release" "argocd" {
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
   version          = "6.6.0"
-  namespace        = argocd
+  namespace        = "argocd"
   create_namespace = true
   wait             = true
   timeout          = 600
