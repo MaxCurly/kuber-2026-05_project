871616efda9c6e877cb7939c7102ba8dcdc70a97
Maxim Kudryashov <m.v.kudryashov@ya.ru>
2026-09-13 21:25:21 +0300

    test

--- terraform/k8s-deploy/resources.tf
+++ terraform/k8s-deploy/resources.tf
@@ -208,18 +208,3 @@ resource "yandex_kubernetes_node_group" "k8s_node_group_1" {
     enabled = var.yc_k8s_node_group_1_workload_identity_federation_enabled
   }
 }
-
-resource "yandex_kubernetes_marketplace_helm_release" "gwin" {
-  cluster_id      = yandex_kubernetes_cluster.k8s_cluster.id
-  name            = "gwin"
-  namespace       = "gwin-system"
-  product_version = "f2e04077v04sobds7gkt"
-  user_values = {
-    "controller.folderId"                                                     = var.yc_folder_id
-    "controller.ycServiceAccount.workloadIdentityFederation.serviceAccountID" = yandex_iam_service_account.otus-kuber-2026-05-admin-sa.id
-    "controller.defaultBalancerSubnets" = yamlencode([
-      yandex_vpc_subnet.subnet_1.id,
-      yandex_vpc_subnet.subnet_2.id,
-    ])
-  }
-}
