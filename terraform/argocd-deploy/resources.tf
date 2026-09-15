3d80010abca3b5dd5cc76c39964053374986c8c9
Maxim Kudryashov <m.v.kudryashov@ya.ru>
2026-09-15 23:39:43 +0300

    test

--- terraform/argocd-deploy/resources.tf
+++ terraform/argocd-deploy/resources.tf
@@ -2,21 +2,11 @@ resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "10.9.1" # Для этого чарта используется global.image
+  version          = "10.9.1"
   namespace        = "argocd"
   create_namespace = true
   wait             = true
   timeout          = 600
-
-  values = [
-    yamlencode({
-      global = {
-        image = {
-          repository = "cr.yandex/cn1vj9hg7fv2jlquf8hj/argoproj/argocd"
-        }
-      }
-    })
-  ]
 }
 
 resource "kubectl_manifest" "root_app" {
