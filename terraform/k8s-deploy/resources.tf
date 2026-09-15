8d989fe033285fb518f2268e7157f8ee275d4d54
Maxim Kudryashov <m.v.kudryashov@ya.ru>
2026-09-15 22:03:15 +0300

    test

--- terraform/k8s-deploy/resources.tf
+++ terraform/k8s-deploy/resources.tf
@@ -17,11 +17,11 @@ resource "yandex_iam_service_account" "sa_node" {
   folder_id   = var.yc_folder_id
 }
 
-resource "yandex_iam_service_account" "sa_gwin" {
-  name        = var.yc_sa_gwin_name
-  description = var.yc_sa_gwin_name
-  folder_id   = var.yc_folder_id
-}
+#resource "yandex_iam_service_account" "sa_gwin" {
+#  name        = var.yc_sa_gwin_name
+#  description = var.yc_sa_gwin_name
+#  folder_id   = var.yc_folder_id
+#}
 
 
 resource "yandex_resourcemanager_folder_iam_member" "sa_cluster_role" {
@@ -36,21 +36,21 @@ resource "yandex_resourcemanager_folder_iam_member" "sa_cluster_role" {
   member    = "serviceAccount:${yandex_iam_service_account.sa_cluster.id}"
 }
 
-resource "yandex_resourcemanager_folder_iam_member" "sa_gwin_role" {
-  for_each = toset([
-    "alb.editor",
-    "vpc.publicAdmin",
-    "certificate-manager.certificates.downloader",
-    "certificate-manager.editor",
-    "compute.viewer",
-    "k8s.viewer",
-    "smart-web-security.editor",
-    "logging.writer"
-  ])
-  folder_id = var.yc_folder_id
-  role      = each.key
-  member    = "serviceAccount:${yandex_iam_service_account.sa_gwin.id}"
-}
+#resource "yandex_resourcemanager_folder_iam_member" "sa_gwin_role" {
+#  for_each = toset([
+#    "alb.editor",
+#    "vpc.publicAdmin",
+#    "certificate-manager.certificates.downloader",
+#    "certificate-manager.editor",
+#    "compute.viewer",
+#    "k8s.viewer",
+#    "smart-web-security.editor",
+#    "logging.writer"
+#  ])
+#  folder_id = var.yc_folder_id
+#  role      = each.key
+#  member    = "serviceAccount:${yandex_iam_service_account.sa_gwin.id}"
+#}
 
 resource "yandex_resourcemanager_folder_iam_member" "sa_node_role" {
   for_each = toset([
